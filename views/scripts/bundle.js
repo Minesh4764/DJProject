f559d3c82ec1b69c7e6e25d94f52636fb84c2a74
minesh20872 <MineshPatel2002@gmail.com>
2017-01-09 21:43:21 -0500

    intial commit

--- views/scripts/bundle.js
+++ views/scripts/bundle.js
@@ -54542,39 +54542,19 @@ var Home = React.createClass({displayName: "Home",
 					   React.createElement("div", {className: "span12"}, 
 
 						   React.createElement("p", null, 
-							   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+							   "DJJB is owned and operated by Bhavin & Minesh Patel.   We are professional DJ’s that provide services at Weddings, Receptions, New Year Party, Birthdays, Dances, Garba / Dadia, Sangeet, and other Social Events. We make your event more memorable, enjoyable, beautiful, and successful. We maintain considerable volume level so everyone can enjoy the music while guest can have clear conversations with other guest at the party.   All our equipment is state of the art and top of the line. We are constantly upgrading to stay ahead of the industry standards. We own the best in wireless microphone systems, digital equipment and programs, mixers, and speaker systems. Brand names such as Shure, Numark, Behringer and QSC, are seen in use. Our equipment is always set up neatly and remains that way throughout the event. There are no messy wires hanging out all over the place. It not only sounds great, but we purposely keep it low profile.   Combining the right blend of personality and audience involvement is what we do. We come dressed appropriately for every occasion. This is done in an unobtrusive manner while keeping ourselves out of the spotlight. We will handle all introductions, announcements, and coordinate with your photographer and videographer to be sure they are in the right position to get all your pictures and to keep them informed of any special events happening throughout the affair. We also coordinate with the caterers and inform the guests when the food is available, and will help to release the tables, if need be, to avoid a bottleneck at the food line.   Our music library consists of over 150,000 songs (750 GB) and covers all genres and all styles fitting all tastes and age groups: We are continuously adding to our collection as well.   Equipment set-up and teardown, travel, unlimited consultations in person or by email or phone, and the actual performance time are all included in the contracted price.   Some will charge less because they don’t provide the skilled type of service that we do. We have that experience and talent taken from the many years we have been doing this. Many of the lesser expensive DJ’s are beginners and do this part-time. They may actually be practicing their skills at your event. Having Bhavin and his company at your event, you are getting a mature, personable, and true professional DJ/MC with our huge selection of music, karaoke, and high-end sound system.   Some DJ’s and most all bands do require breaks. However, we do not. We don’t smoke, sit, leave the room, or take breaks. We remain professional and available to you at all times."
 						   )
-
 					   )
 
 				   ), 
 
-				   React.createElement("div", {className: "row-fluid"}, 
-
-					   React.createElement("div", {className: "span6"}, 
-
-						   React.createElement("p", null, 
-							   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
-						   )
-
-					   ), 
-
-					   React.createElement("div", {className: "span6"}, 
-
-						   React.createElement("p", null, 
-							   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
-						   )
-
-					   )
-
-				   ), 
 
 				   React.createElement("div", {className: "row-fluid"}, 
 
 					   React.createElement("div", {className: "span4"}, 
 						   React.createElement("div", {className: "icons-box"}, 
 							   React.createElement("i", {className: "fa-icon-beaker"}), 
-							   React.createElement("h3", null, "Mobile"), 
+							   React.createElement("h3", null, "Dance"), 
 							   React.createElement("p", null, 
 								   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
 							   ), 
@@ -54584,8 +54564,9 @@ var Home = React.createClass({displayName: "Home",
 
 					   React.createElement("div", {className: "span4"}, 
 						   React.createElement("div", {className: "icons-box"}, 
-							   React.createElement("i", {className: "fa-icon-thumbs-up"}), 
-							   React.createElement("h3", null, "Web"), 
+
+							   React.createElement("i", {className: "fa fa-music", "aria-hidden": "true"}), 
+							   React.createElement("h3", null, "Music"), 
 							   React.createElement("p", null, 
 								   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
 							   ), 
@@ -54596,7 +54577,7 @@ var Home = React.createClass({displayName: "Home",
 					   React.createElement("div", {className: "span4"}, 
 						   React.createElement("div", {className: "icons-box"}, 
 							   React.createElement("i", {className: "fa-icon-heart-empty"}), 
-							   React.createElement("h3", null, "Responsive"), 
+							   React.createElement("h3", null, "Entertainment"), 
 							   React.createElement("p", null, 
 								   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
 							   ), 
